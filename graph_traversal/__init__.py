5f87419af00fb090b11d52f1aaf8eec4e0f779af
Bart Enkelaar <benkelaar@bol.com>
2022-04-03 22:37:02 +0200

    Slightly improve local parameter naming

--- graph_traversal/__init__.py
+++ graph_traversal/__init__.py
@@ -4,7 +4,7 @@ from adaptation_pathway import AdaptationNode, AdaptationPath
 
 
 def find_most_flexible_path(
-        current_node: AdaptationNode,
+        node: AdaptationNode,
         pathway: AdaptationPath = None,
         flexibility: int = 0
 ) -> tuple[AdaptationPath, int]:
@@ -15,21 +15,21 @@ def find_most_flexible_path(
 
     For equal flexibilities the algorithm is stable, so should return the first found path with that flexibility.
 
-    :param current_node: root node for the path to test for flexibility
+    :param node: root node for the path to test for flexibility
     :param pathway: path taken so far, defaults to `[current_node]`
     :param flexibility: Amount of choices seen so far, defaults to 0
     :return: a pair of the first most flexible path seen and its total flexibility
     """
 
-    followed_path = [current_node] if pathway is None else pathway + [current_node]
+    followed_path = [node] if pathway is None else pathway + [node]
     # Exit condition
-    if current_node.is_leaf_node():
+    if node.is_leaf_node():
         return followed_path, flexibility
 
     # Otherwise, recurse for all available choices
-    new_flexibility = flexibility + current_node.choice_count()
-    choice_path_flexibilities = map(lambda node: find_most_flexible_path(node, followed_path, new_flexibility),
-                                    current_node.choices)
+    new_flexibility = flexibility + node.choice_count()
+    choice_path_flexibilities = map(lambda new_node: find_most_flexible_path(new_node, followed_path, new_flexibility),
+                                    node.choices)
 
     # And select first tuple with the highest flexibility count
     return max(choice_path_flexibilities, key=lambda pf_pair: pf_pair[1])
